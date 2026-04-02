ebbf4de273c8fc5a345ded14d80c8573811eef35
Vinayak S <oreva.vinayak@gmail.com>
2026-04-02 20:10:02 +0530

    feat: initial springBoot

---
+++ src/main/java/com/example/test_ecom_project/TestEcomProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.test_ecom_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestEcomProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestEcomProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/test_ecom_project/TestEcomProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.test_ecom_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestEcomProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
