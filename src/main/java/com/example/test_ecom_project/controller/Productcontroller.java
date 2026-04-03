8cdbba92f9ed70c4e1d9c33a957611d30237598d
Vinayak S <oreva.vinayak@gmail.com>
2026-04-03 20:43:04 +0530

    fix: cross-origin resource sharing error

--- src/main/java/com/example/test_ecom_project/controller/Productcontroller.java
+++ src/main/java/com/example/test_ecom_project/controller/Productcontroller.java
@@ -4,13 +4,14 @@ import com.example.test_ecom_project.model.Products;
 import com.example.test_ecom_project.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
-
+@CrossOrigin
 @RestController
 @RequestMapping("/api")
 public class Productcontroller {
