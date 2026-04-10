6f47e46115e4748f8048348d0bbe0451490b2f67
Vinayak S <oreva.vinayak@gmail.com>
2026-04-10 19:41:51 +0530

    feat: add product creation API with image upload support

--- src/main/java/com/example/test_ecom_project/controller/Productcontroller.java
+++ src/main/java/com/example/test_ecom_project/controller/Productcontroller.java
@@ -2,11 +2,14 @@ package com.example.test_ecom_project.controller;
 
 import com.example.test_ecom_project.model.Products;
 import com.example.test_ecom_project.service.ProductService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.util.List;
 
@@ -15,6 +18,7 @@ import java.util.List;
 @RequestMapping("/api")
 public class Productcontroller {
 
+    private static final Logger log = LoggerFactory.getLogger(Productcontroller.class);
     @Autowired
     ProductService service;
 
@@ -32,6 +36,17 @@ public class Productcontroller {
         }
         else{
             return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
+    @PostMapping("/product")
+    public ResponseEntity<?> addProduct(@RequestPart Products product,
+                                        @RequestPart MultipartFile imageFile){
+        try{
+            Products product1 = service.addProduct(product,imageFile);
+            return new ResponseEntity<>(product1,HttpStatus.CREATED);
+        }
+        catch(Exception e){
+            return new ResponseEntity<>(e.getMessage(),HttpStatus.INTERNAL_SERVER_ERROR);
         }
     }
 }
