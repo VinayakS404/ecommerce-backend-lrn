9ab56fdcf0b6b5d31d0c89705227f6d2ca549f05
Vinayak S <oreva.vinayak@gmail.com>
2026-04-10 19:45:43 +0530

    feat:add setters and add image attributes

--- src/main/java/com/example/test_ecom_project/model/Products.java
+++ src/main/java/com/example/test_ecom_project/model/Products.java
@@ -1,10 +1,7 @@
 package com.example.test_ecom_project.model;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -14,20 +11,42 @@ import java.util.Date;
 
 @Entity
 @Data
-@AllArgsConstructor
 @NoArgsConstructor
+@AllArgsConstructor
 public class Products {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private int id;
+    private Integer id;
     private String name;
     private String description;
     private String  brand;
     private BigDecimal price;
     private String  category;
 
-    @JsonFormat(shape = JsonFormat.Shape.STRING,pattern = "dd-MM-yyyy")
     private Date releaseDate;
     private Boolean available;
-    private int quantity;
+    private Integer quantity = 0;
+
+    private String imageName;
+    private String imageType;
+
+    @Lob
+    private byte[] imageData;
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public void setImageType(String imageType) {
+        this.imageType = imageType;
+    }
+
+    public void setImageName(String imageName) {
+        this.imageName = imageName;
+    }
+
+    public void setImageData(byte[] imageData) {
+        this.imageData = imageData;
+    }
 }
